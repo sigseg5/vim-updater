2937790134a4a8166f1efc7df6a88a678e65f544
Kirill <kirill.parhomenko@gmail.com>
2019-09-01 19:13:10 +0500

    ADD: os checker utilities

---
+++ src/Utilities/os_checker.py
@@ -0,0 +1,13 @@
+from sys import platform
+
+
+def check_os():
+    # BUG?: platform == "linux2" - true on macOS
+    if platform == "linux":
+        return "linux"
+    elif platform == "darwin":
+        return "mac"
+    elif platform == "win32":
+        return "win"
+    else:
+        return "other"
