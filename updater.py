f8c8960e62bb316bb5aa6394ca3b9ce349707a52
Kirill <kirill.parhomenko@gmail.com>
2019-09-01 20:27:11 +0500

    ADD: main script

--- updater.py
+++ updater.py
@@ -1,5 +1,23 @@
+from sys import exit as sys_exit
+
 from src.Utilities.os_checker import check_os
 from src.Utilities.git_checker import check_git_on_device
+from src.Utilities.git_downloader import openBrowser
 
-test = check_git_on_device()
-print(test)
+
+UPDATER_VER = "0.0.0"
+OS_TYPE = check_os()
+GIT_STATUS = check_git_on_device()
+GIT_DOWNLOAD_PAGE = "http://git-scm.com/"
+
+print("""
+    VIM updater {updater_version}
+    OS: {os_type}
+    Git status: {git_status}
+""".format(updater_version=UPDATER_VER,
+           os_type=OS_TYPE,
+           git_status=GIT_STATUS))
+
+if str(GIT_STATUS).count("git version") != 1:
+    openBrowser(GIT_DOWNLOAD_PAGE)
+    sys_exit(0)
