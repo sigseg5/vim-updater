3a7dc172097d28e74dcc54ed2b633562c57fa75c
Kirill <kirill.parhomenko@gmail.com>
2019-09-01 21:40:48 +0500

    ADD: vim version checker template

---
+++ src/Utilities/current_vim_version_checker.py
@@ -0,0 +1,6 @@
+def check_current_vim_version_in_system():
+    print("current vim version in system")
+
+
+def check_current_vim_version_in_src_folder():
+    print("current vim version in src folder")
