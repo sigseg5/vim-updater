d04c817fe13590ba65dffda7c9dbc18fefec302f
Kirill <kirill.parhomenko@gmail.com>
2019-09-01 19:12:43 +0500

    ADD: main files

---
+++ src/Utilities/__init__.py
@@ -0,0 +1,2 @@
+from src.Utilities import os_checker
+from src.Utilities import git_checker
---
+++ updater.py
@@ -0,0 +1,5 @@
+from src.Utilities.os_checker import check_os
+from src.Utilities.git_checker import check_git_on_device
+
+test = check_git_on_device()
+print(test)
