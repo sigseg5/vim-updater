63c2a039bcd30df6d61d4ffbc3b831a131190ab1
Kirill <kirill.parhomenko@gmail.com>
2019-09-01 21:28:07 +0500

    UPD: file logic in main script

--- updater.py
+++ updater.py
@@ -1,16 +1,18 @@
 from sys import exit as sys_exit
 from os import path
+from os import mkdir
 
 from src.Utilities.os_checker import check_os
 from src.Utilities.git_checker import check_git_on_device
 from src.Utilities.git_downloader import openBrowser
-
 
 UPDATER_VER = "0.0.0"
 OS_TYPE = check_os()
 GIT_STATUS = check_git_on_device()
 GIT_DOWNLOAD_PAGE = "http://git-scm.com/"
 USER_FOLDER = path.expanduser("~")
+UPDATER_DIR = "/.vim_updater"
+VIM_FOLDER = "/vim"
 
 print("""
     VIM updater version: {updater_version}
@@ -24,10 +26,41 @@ if str(GIT_STATUS).count("git version") != 1:
     openBrowser(GIT_DOWNLOAD_PAGE)
     sys_exit(0)
 
-if path.isdir(USER_FOLDER) and path.exists(USER_FOLDER + "/.vim_updater"):
-    print("work folder and updater dir  here, check vim src folder")
-elif path.isdir(USER_FOLDER) and not path.exists(USER_FOLDER + ".vim_updater"):
-    print("work folder here, create updater dir and vim src folder")
-else:
-    print(f"{'error with user folder:'} {USER_FOLDER}")
+if path.isdir(USER_FOLDER) and path.exists(USER_FOLDER + UPDATER_DIR):
+    print("work folder and updater dir here, check vim src folder")
+    if path.exists(USER_FOLDER + UPDATER_DIR + VIM_FOLDER):
+        print("cd vim & start git clone")
+    else:
+        try:
+            mkdir(USER_FOLDER + UPDATER_DIR + VIM_FOLDER)
+        except FileExistsError:
+            print("Directory {user_folder}{updater_dir}{vim_folder} already exists".format(user_folder=USER_FOLDER,
+                                                                                           updater_dir=UPDATER_DIR,
+                                                                                           vim_folder=VIM_FOLDER))
+            sys_exit(1)
+        print("cd vim & start git clone")
 
+elif path.isdir(USER_FOLDER) and not path.exists(USER_FOLDER + UPDATER_DIR):
+
+    try:
+        mkdir(USER_FOLDER + UPDATER_DIR)
+        print("Directory {user_folder}{updater_dir} successfully created".format(user_folder=USER_FOLDER,
+                                                                                 updater_dir=UPDATER_DIR))
+    except FileExistsError:
+        print("Directory {user_folder}{updater_dir} already exists".format(user_folder=USER_FOLDER,
+                                                                           updater_dir=UPDATER_DIR))
+        sys_exit(1)
+
+    print("try to create vim folder")
+    try:
+        mkdir(USER_FOLDER + UPDATER_DIR + VIM_FOLDER)
+    except FileExistsError:
+        print("Directory {user_folder}{updater_dir}{vim_folder} already exists".format(user_folder=USER_FOLDER,
+                                                                                       updater_dir=UPDATER_DIR,
+                                                                                       vim_folder=VIM_FOLDER))
+        sys_exit(1)
+    print("cd vim & start git clone")
+
+else:
+    print("error with user folder: {user_folder}".format(user_folder=USER_FOLDER))
+    sys_exit(1)
