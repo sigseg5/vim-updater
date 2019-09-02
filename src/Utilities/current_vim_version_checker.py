1d3ab2fc3417e40aba3941c27ac024b7a450d156
Kirill <kirill.parhomenko@gmail.com>
2019-09-02 18:57:07 +0500

    UPD: try to get included patches from version.c, WIP

--- src/Utilities/current_vim_version_checker.py
+++ src/Utilities/current_vim_version_checker.py
@@ -18,16 +18,25 @@ def check_current_vim_version_in_system():
 
 
 def check_current_vim_version_in_src_folder(path_to_vim_folder):
+    included_patches_flag = False
     print("current vim version in src folder")
     # TODO: add 'not' to statement
-    # if path.isdir(path_to_vim_folder):
+    # if path.isdir(path_to_vim_folder, flag):
     if not path.isdir(path_to_vim_folder):
         sys_exit(1)
-    with open(path_to_vim_folder + "/src/version.c", "r") as src_version:
-        last_patch_flag = False
-        additional_line_count = 0
-        for line in src_version:
-            if "static int included_patches[]" in line:
-                last_patch_flag = True
-            if last_patch_flag:
-                additional_line_count = 2
+
+    included_patch_output = ""
+    try:
+        with open(path_to_vim_folder + "/src/version.c", "r") as src_version:
+            for line in src_version:
+                if "static int included_patches[]" in line:
+                    included_patches_flag = True
+                    if included_patches_flag:
+                        included_patch_output += line
+                        if "," in line:
+                            included_patches_flag = False
+    except FileNotFoundError:
+        print("error")
+        sys_exit(1)
+
+    print(included_patch_output)
