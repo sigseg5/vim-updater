0dbd98fcb36ab087a058cc38a4b56ab13f2f9798
Kirill <kirill.parhomenko@gmail.com>
2019-09-02 16:39:18 +0500

    ADD: system vim version cheker

--- src/Utilities/__init__.py
+++ src/Utilities/__init__.py
@@ -1,3 +1,5 @@
 from src.Utilities import os_checker
 from src.Utilities import git_checker
 from src.Utilities import git_downloader
+from src.Utilities import git_utilities
+from src.Utilities import current_vim_version_checker
--- src/Utilities/current_vim_version_checker.py
+++ src/Utilities/current_vim_version_checker.py
@@ -1,5 +1,19 @@
+from os import system
+import subprocess
+
+
 def check_current_vim_version_in_system():
     print("current vim version in system")
+    command = "vim --version"
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout = []
+
+    for line in proc.stdout:
+        if b"VIM - Vi IMproved" in line:
+            stdout.append(line)
+        if b"Included patches" in line:
+            stdout.append(line)
+    return "".join(map(bytes.decode, stdout))
 
 
 def check_current_vim_version_in_src_folder():
