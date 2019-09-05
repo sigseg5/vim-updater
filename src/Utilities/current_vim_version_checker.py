b23497322a4641eb397bfb18244fde0e58091ffd
Kirill <kirill.parhomenko@gmail.com>
2019-09-05 19:40:37 +0500

    Started adding function to check major and minor vim version

--- src/Utilities/current_vim_version_checker.py
+++ src/Utilities/current_vim_version_checker.py
@@ -2,6 +2,8 @@ import subprocess
 from os import path
 from sys import exit as sys_exit
 
+# TODO: add try except
+
 
 def check_current_vim_version_in_system():
     print("current vim version in system")
@@ -17,14 +19,31 @@ def check_current_vim_version_in_system():
     return "".join(map(bytes.decode, stdout))
 
 
-def check_current_vim_version_in_src_folder(path_to_vim_folder):
-    print("current vim version in src folder")
+def check_current_vim_version_in_src(path_to_vim_folder):
     # TODO: add 'not' to statement
-    # if path.isdir(path_to_vim_folder, flag):
+    # if path.isdir(path_to_vim_folder):
     if not path.isdir(path_to_vim_folder):
         sys_exit(1)
 
-    included_patch_output = ""
+    # check major and minor version
+    major_and_minor_version = ""
+    try:
+        with open(path_to_vim_folder + "/Makefile", "r") as make_file:
+            first_minor_in_text_flag = False
+            for line in make_file:
+                if not first_minor_in_text_flag:
+                    if line.find("MAJOR") != -1:
+                        major_and_minor_version += line
+                    if line.find("MINOR") != -1:
+                        major_and_minor_version += line
+                        first_minor_in_text_flag = True
+            print(major_and_minor_version)
+    except FileNotFoundError:
+        print("error")
+        sys_exit(1)
+
+    # check included patches
+    included_patches = ""
     try:
         first_version_in_file_flag = False
         with open(path_to_vim_folder + "/src/version.c", "r") as src_version:
@@ -34,8 +53,8 @@ def check_current_vim_version_in_src_folder(path_to_vim_folder):
                 if first_version_in_file_flag:
                     if line.find(",") != -1:
                         first_version_in_file_flag = False
-                        included_patch_output += line
-        return included_patch_output.strip().strip(",")
+                        included_patches += line
+        return included_patches.strip().strip(",")
     except FileNotFoundError:
         print("error")
         sys_exit(1)
