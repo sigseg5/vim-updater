f5812f8ad40ca421e287b17d9e03149bfe32d0b1
Kirill <kirill.parhomenko@gmail.com>
2019-09-05 19:39:23 +0500

    Update tooltip

--- src/Utilities/os_checker.py
+++ src/Utilities/os_checker.py
@@ -2,7 +2,7 @@ from sys import platform
 
 
 def check_os():
-    # TODO: FIX BUG: platform == "linux2" - true on macOS
+    # fixme: platform == "linux2" return true on macOS
     if platform == "linux":
         return "linux"
     elif platform == "darwin":
