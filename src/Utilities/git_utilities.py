243ac90ebbe33653a941b067a58207c68d5955c9
Kirill <kirill.parhomenko@gmail.com>
2019-09-06 19:58:28 +0500

    Updated git functions

--- src/Utilities/git_utilities.py
+++ src/Utilities/git_utilities.py
@@ -1,26 +1,26 @@
 from sys import exit as sys_exit
 import subprocess
-
 # can only clone and pull from github repo
 
-def consoleOutputParse(console_command):
-    command = "vim --version"
+
+def consoleOutputParse(console_command, repo):
+    command = "git {cmd} {repo}".format(cmd=console_command,
+                                        repo=repo)
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout = []
 
     for line in proc.stdout:
-        if b"VIM - Vi IMproved" in line:
-            stdout.append(line)
-        if b"Included patches" in line:
-            stdout.append(line)
-    return "".join(map(bytes.decode, stdout))
+        print("".join(map(bytes.decode, line)))
+    pass
+
 
 def git_action(command):
     VIM_REPO = "https://github.com/vim/vim.git"
     if command == "clone":
         print("clone")
+        print(consoleOutputParse(command, VIM_REPO))
     elif command == "pull":
         print("pull")
+        print(consoleOutputParse(command))
     else:
         print("command error")
         sys_exit(1)
