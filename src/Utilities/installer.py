2350b043fc3b059db0d8b42ac14296cfa98d51fc
Kirill <kirill.parhomenko@gmail.com>
2019-09-12 10:03:41 +0500

    Added template for make action

--- src/Utilities/installer.py
+++ src/Utilities/installer.py
@@ -1,14 +1,30 @@
 import getpass
+import subprocess
+from sys import exit as sys_exit
 
 
-def make_action():
-    print("make")
-    try:
-        passw = getpass.getpass(prompt='Password: ', stream=None)
-    except getpass.GetPassWarning:
-        print("echo")
-    print(passw)
+def make_action(*args):
+    # TODO: add make check in system
+
+    if len(args) == 2 and args[0] == "make":
+        print("just make")
+        command = "make"
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=args[1])
+
+    elif len(args) == 4 and args[0] == "sudo" and args[1] == "make" and args[2] == "install":
+        print("make install")
+        command = "sudo make install"
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=args[3])
+
+    else:
+        print("args error")
+        sys_exit(1)
 
 
-def make_install_action():
-    print("make install")
+
+    # try:
+    #     passw = getpass.getpass(prompt='Password: ', stream=stderr)
+    # except getpass.GetPassWarning:
+    #     print("echo")
+    # print(passw)
+
