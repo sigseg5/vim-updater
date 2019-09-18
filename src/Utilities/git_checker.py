d82908ff7d314901062c728675a03e8be6398e8e
Kirill <kirill.parhomenko@gmail.com>
2019-09-18 22:09:06 +0500

    Refactor git checker

--- src/Utilities/git_checker.py
+++ src/Utilities/git_checker.py
@@ -2,16 +2,5 @@ import subprocess
 
 
 def check_git_on_device():
-    command = "git --version"
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    stdout = []
-
-    while True:
-        outputLine = proc.stdout.readline()
-        stdout.append(outputLine)
-
-        if outputLine == b'' and proc.poll() is not None:
-            break
-
-    return "".join(map(bytes.decode, stdout))
+    proc = subprocess.check_output(["git", "--version"])
+    return proc.decode()
