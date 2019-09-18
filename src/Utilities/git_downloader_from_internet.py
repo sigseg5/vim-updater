4dab0f7cfe7212cc7333b8b860603540f690b8b1
Kirill <kirill.parhomenko@gmail.com>
2019-09-18 22:16:16 +0500

    Import optimisation

--- src/Utilities/git_downloader_from_internet.py
+++ src/Utilities/git_downloader_from_internet.py
@@ -1,9 +1,9 @@
-import webbrowser
+from webbrowser import open_new_tab
 
 
 def openBrowser(url):
     try:
-        webbrowser.open_new_tab(url)
+        open_new_tab(url)
     except AttributeError:
         print("AttributeError\nCan't open browser")
         print("Open link to download git for you system\nLink: http://git-scm.com/")
