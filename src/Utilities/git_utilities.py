a5415119e38649d422c63dc2a30a24fdc05c1490
Kirill <kirill.parhomenko@gmail.com>
2019-09-18 22:03:30 +0500

    Refactor git pull function

--- src/Utilities/git_utilities.py
+++ src/Utilities/git_utilities.py
@@ -15,16 +15,10 @@ def git_action(*args):
 
     if len(args) == 2 and args[0] == "pull":
         print("git pull")
-        command = "git {cmd}".format(cmd=args[0])
+        # command = "git {cmd}".format(cmd=args[0])
         # reset_command = "git reset --hard"
 
-        # rest_proc = subprocess.Popen(reset_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=args[1])
-
-        # print(rest_proc.stdout.read(350).strip().decode("utf-8"))
-
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=args[1])
-
-        print(proc.stdout.read(350).strip().decode("utf-8"))
+        _ = subprocess.call(["git", args[0]], cwd=args[1])
 
     elif len(args) == 3 and args[0] == "clone":
         print("git clone {repo} {path}".format(repo=args[1], path=args[2]))
@@ -33,5 +27,4 @@ def git_action(*args):
     else:
         print("trouble with args")
         sys_exit(1)
-
     pass
