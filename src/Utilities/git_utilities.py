390c2b1b83c14eb76cc9db4b5f5d86d873d26e92
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2020-07-10 22:29:59 +0500

    Fixed backward compatibility with old git versions

--- src/Utilities/git_utilities.py
+++ src/Utilities/git_utilities.py
@@ -11,7 +11,7 @@ def check_git_on_device():
 def git_action(*args):
     if len(args) == 2 and args[0] == "pull":
         pull_proc_out = check_output(["git", args[0]], cwd=args[1])
-        if (pull_proc_out.decode('utf-8')).count("Already up-to-date.") == 1:
+        if (pull_proc_out.decode('utf-8')).count("Already up-to-date.") == 1 or (pull_proc_out.decode('utf-8')).count("Already up to date.") == 1:
             return True
 
     elif len(args) == 3 and args[0] == "clone":
